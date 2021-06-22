fdfdedfb24d46a82a7f0af975d673267fb0c6716
info <58048398+kaushikappani@users.noreply.github.com>
2021-06-22 12:00:36 +0530

    remove test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-const { encrypt, decode } = require('./app');
-
-let encoded_text = encrypt("hello world!", 2811)
-console.log(encoded_text)
-let decode_text = decode("khoor zruog!", 2811)
-console.log(decode_text)
